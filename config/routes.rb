28633f3547e9ff80159700bb896eb853a440f2a5
@stux <info@mstdn.social>
2020-08-05 20:15:39 +0200

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -496,6 +496,10 @@ Rails.application.routes.draw do
   get '/about',        to: 'about#show'
   get '/about/more',   to: 'about#more'
   get '/terms',        to: 'about#terms'
+  get '/apps',        to: 'about#apps'
+  get '/team',        to: 'about#team'
+  get '/funding',        to: 'about#funding'
+  get '/discover',       to: 'about#discover'
 
   match '/', via: [:post, :put, :patch, :delete], to: 'application#raise_not_found', format: false
   match '*unmatched_route', via: :all, to: 'application#raise_not_found', format: false
