ae36e44081f2cbd4660311abf34415c0241e62f1
@stux <info@mstdn.social>
2020-08-23 15:23:23 +0200

    Source url

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -33,11 +33,11 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY') { 'tootsuite/mastodon' }
+      ENV.fetch('GITHUB_REPOSITORY') { 'Mastodon/mstdn' }
     end
 
     def source_base_url
-      ENV.fetch('SOURCE_BASE_URL') { "https://github.com/#{repository}" }
+      ENV.fetch('SOURCE_BASE_URL') { "https://stuxos.org/#{repository}" }
     end
 
     # specify git tag or commit hash here
